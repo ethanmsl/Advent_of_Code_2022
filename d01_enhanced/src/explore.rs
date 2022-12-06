2946d99b1945fecec0f67371f599a6fafe05dc2f
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-06 05:29:20 -0800

    !! Doc-Tests won't run without a `lib.rs` file and mod-declaration in it
    Adding `lib.rs` *&&* referencing the module makes the no module ref test work, but the module ref test
    does not currently (work)

--- d01_enhanced/src/explore.rs
+++ d01_enhanced/src/explore.rs
@@ -3,6 +3,33 @@
 //! Just here so I can play with input and output types
 //! And only linked to main.rs for exploratory run purposes
 
-pub fn print_to_test_sourcing() {
+/// # just a rust file connection test
+/// print a line from `main.rs` to make sure this file is being sourced
+/// ## Example
+/// print_to_test_sourcing();
+/// (lol)
+///
+///
+/// The next lines present detailed documentation. Code blocks start with
+/// triple backquotes and have implicit `fn main()` inside
+/// and `extern crate <cratename>`. 
+///
+/// ```
+/// let result = 2+3;
+/// assert_eq!(result, 5);
+/// ```
+pub fn print_to_test_sourcing(num: i32) {
     println!("------------function defined in Explore.rs------------");
+    println!("The input number is: {}", num);
+}
+
+
+/// *le sigh*  
+// trying to get documentation tests up and running
+/// ```
+/// let result = explore::add_three(1, 2, 3);
+/// assert_eq!(result, 6);
+/// ```
+pub fn add_three (n1: i32, n2: i32, n3: i32) -> i32 {
+    n1 + n2 + n3
 }
--- d01_enhanced/src/main.rs
+++ d01_enhanced/src/main.rs
@@ -1,5 +1,10 @@
 mod explore;
 
 fn main() {
-    explore::print_to_test_sourcing();
+    let a = 12 + 3;
+    let b = 3 + a;
+    let c: i32 = 17;
+
+    let sum = explore::add_three(a, b, c);
+    explore::print_to_test_sourcing(sum);
 }
