e9d4a8579500b5415c0dfd90995f77c5a7ce052c
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-05 20:52:19 -0800

    Day_01 - read-in file, parse, sum, sort

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,3 +1,44 @@
+
+// read input from a text file
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+
+    let input = 
+        fs::read_to_string("input")
+        .expect("Error reading input file");
+    let lines = input.lines();
+
+    // []
+    let mut elf_vec: Vec<u64> = Vec::new();
+    
+    // for line in lines.take(100) {
+    //      match line {
+    //          "" => println!(" '', {}", line),
+    //          _ => println!(" _, {}", line),
+    //      }
+    // }
+
+    let mut accumulator: u64 = 0;
+    for line in lines {
+        match line {
+            "" => {
+                elf_vec.push(accumulator);
+                accumulator = 0;
+            }
+            _ => {accumulator += line.parse::<u64>().unwrap();}
+        }
+    }
+
+    // retun max value
+    let max_elf_val = elf_vec.iter().max().unwrap();
+    println!("max_elf_val: {}", max_elf_val);
+
+    // return sum of top 3 values of vector
+    // apparently I can't chain the sort & reverse since they don't return the vector
+    elf_vec.sort();
+    elf_vec.reverse();
+    let sum_top_3 = elf_vec[0..3].iter().sum::<u64>();
+    println!("sum_top_3: {}", sum_top_3);
+
 }
