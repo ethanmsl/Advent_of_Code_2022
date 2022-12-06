c5cab8cdff071c7cad270e76382001344ac898ab
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-06 06:04:11 -0800

    Insight: Doc-Tests use lib.rs as externs, (reg-)Tests use local scope
    Private Functions **cannot** be doc-tested.
    You *can* tell documentation to skip testing (in order to get the same
    syntax/coloring in presentation) -- but that creates muddyness about
    what is accuracy checked and what isn't -- I would prefer clarity there
    to syntax coloring.

--- d01_enhanced/src/explore.rs
+++ d01_enhanced/src/explore.rs
@@ -25,11 +25,45 @@ pub fn print_to_test_sourcing(num: i32) {
 
 
 /// *le sigh*  
-// trying to get documentation tests up and running
+/// trying to get documentation tests up and running
 /// ```
+/// use d01_enhanced::explore;
 /// let result = explore::add_three(1, 2, 3);
 /// assert_eq!(result, 6);
 /// ```
 pub fn add_three (n1: i32, n2: i32, n3: i32) -> i32 {
     n1 + n2 + n3
+}
+
+/// *le sigh* cont'd  
+///
+/// creating a private function to check that local testing still works with it
+/// even though documentation testing apparently does not
+/// ```
+/// use d01_enhanced::explore;
+/// let result = explore::mult_three(1, 2, 3);
+/// assert_eq!(result, 7);
+/// ```
+fn mult_three (n1: i32, n2: i32, n3: i32) -> i32 {
+    n1 * n2 * n3
+}
+
+// some tests to check that scoping here is local
+// , contrary to documentation-tests, which appear to compile the library and then
+// call from it like an external caller.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// using public function
+    fn test_add_three() {
+        assert_eq!(add_three(1, 7, 3), 11);
+    }
+
+    #[test]
+    /// using public function
+    fn test_mult_three() {
+        assert_eq!(mult_three(1, 7, 3), 21);
+    }
 }
--- d01_enhanced/src/lib.rs
+++ d01_enhanced/src/lib.rs
@@ -1,1 +1,8 @@
-mod explore;
+//! This is *only* here to allow documentation tests
+//! Unforunately, it seems that documentation tests are run
+//! by compiling the library and then referncing into it, as per normal.
+//! This means that we can *only* run tests on **Public** functions.
+//! Unlike typical code tests, the documentation tests are *not*
+//! run in the scope of module where they are written.
+
+pub mod explore;
